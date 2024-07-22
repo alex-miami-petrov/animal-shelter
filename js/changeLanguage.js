941c2d8c502320417603bd5badc280659620b9ef
Miami <alex.miami.petrov@gmail.com>
2024-07-22 12:24:15 +0300

    m

--- js/changeLanguage.js
+++ js/changeLanguage.js
@@ -1,32 +1,46 @@
 $(document).ready(function () {
-  $("#language-toggle").click(function () {
-    $("#language-menu").toggle();
+  // Початкове приховання меню
+  $("#language-menu").hide();
+
+  // Клік на кнопку language-toggle
+  $("#language-toggle").click(function (e) {
+    e.stopPropagation(); // Зупиняємо подальше випливання події
+    $("#language-menu").show(); // Показуємо меню language-menu
   });
 
+  // Клік на будь-який елемент в межах language-menu
+  $("#language-menu").click(function (e) {
+    e.stopPropagation(); // Зупиняємо подальше випливання події
+  });
+
+  // Клік поза language-menu ховає його
+  $(document).click(function () {
+    $("#language-menu").hide();
+  });
+
+  // Обробка кліку на посилання в меню
   $("#language-menu a").click(function (e) {
     e.preventDefault();
     var lang = $(this).attr("data-lang");
     changeLanguage(lang);
-    $("#language-menu").hide();
+    $("#language-menu").hide(); // Ховаємо меню після вибору мови
   });
 
   function changeLanguage(lang) {
     // Оновлюємо текст кнопки або іншого елемента з вибором мови
-    $("#language-toggle").text(lang.toUpperCase()); // Приклад: встановлюємо текст кнопки в верхній регістр обраної мови
+    $("#language-toggle").text(lang.toUpperCase());
 
     // Тут ви можете додати свою логіку для зміни контенту на сторінці на обрану мову
     // Наприклад, якщо у вас є AJAX-запит для отримання вмісту на обраній мові:
     $.ajax({
-      url: "your-backend-endpoint", // Замініть це на URL вашого серверу, який повертає вміст на обраній мові
+      url: "your-backend-endpoint",
       method: "GET",
       data: { lang: lang },
       success: function (response) {
-        // Обробка успішного відповіді від сервера
         console.log("Контент на мові " + lang + ":", response);
         // Тут ви можете оновити DOM з отриманим відповідним контентом
       },
       error: function (err) {
-        // Обробка помилки від сервера
         console.error(
           "Помилка при отриманні контенту на мові " + lang + ":",
           err
